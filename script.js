a4404f673c2c1bfe4cf6657f889bf9ad2edba5a9
Md. Nuray Alam Siddik <siddik4x@gmail.com>
2021-01-30 18:35:37 +0600

    confirmation section added

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 function changeTicketCounts(currentTicketCountId, isIncrease) {
-    const currentTicketCount = parseInt(document.getElementById(currentTicketCountId).value);
+    const currentTicketCount = getInputValue(currentTicketCountId);
     let updatedTicketCount = 0; // initially dummy value
-    if (isIncrease == true) {
+    if (isIncrease == true && currentTicketCount >= 0) {
         updatedTicketCount = currentTicketCount + 1;
     }
     else if (currentTicketCount > 0) {
         updatedTicketCount = currentTicketCount - 1;
     }
     document.getElementById(currentTicketCountId).value = updatedTicketCount;
-
-
-    // updating bill section
-    const subTotal = document.getElementById('subTotal');
-    const vat = document.getElementById('vat');
-    const total = document.getElementById('total');
-    const firstClassTicketCount = parseInt(document.getElementById('first-class-current-ticket-count').value);
-    const economyClassTicketCount = parseInt(document.getElementById('economy-current-ticket-count').value);
-    const subTotalCount = firstClassTicketCount * 150 + economyClassTicketCount * 100;
-    const vatCount = Math.round(subTotalCount * 0.1);
-    const totalCount = subTotalCount + vatCount;
-    subTotal.innerText = '$' + subTotalCount;
-    vat.innerText = '$' + vatCount;
-    total.innerText = '$' + totalCount;
-    
+    updateBillSection();
+}
+//update bill secetion function
+function updateBillSection() {
+    const firstClassTicketCount = getInputValue('first-class-current-ticket-count')
+    const economyClassTicketCount = getInputValue('economy-current-ticket-count')
+    if (firstClassTicketCount >= 0 && economyClassTicketCount >= 0) {
+        const subTotalCount = firstClassTicketCount * 150 + economyClassTicketCount * 100;
+        const vatCount = Math.round(subTotalCount * 0.1);
+        const totalCount = subTotalCount + vatCount;
+        // changing the bill section value
+        changeInnerText('subTotal', '$' + subTotalCount);
+        changeInnerText('vat', '$' + vatCount);
+        changeInnerText('total', '$' + totalCount);
+    }
+    else {
+        alert('Please avoid negative input or empty field');
+    }
+}
+//get input value function
+function getInputValue(inputId) {
+    const inputValue = parseFloat(document.getElementById(inputId).value);
+    return Math.round(inputValue);
+}
+//inner text changing function
+function changeInnerText(idToChangeInnerText, valueToSet) {
+    document.getElementById(idToChangeInnerText).innerText = valueToSet;
+}
+//changing display property function
+function changeDisplayProperty(hidingElementId, property) {
+    document.getElementById(hidingElementId).style.display = property;
+}
+// cofiramation function
+function confirmation() {
+    if (document.getElementById('total').innerText == '$00') {
+        alert('To confirm booking you have to add at least one ticket');
+    }
+    else {
+        document.getElementById('form-detail-section').style.display = 'none';
+        changeDisplayProperty('confirmation-section', 'none');
+        document.getElementById('booking-id').innerText = 'Your Booking ID is : ' + Math.ceil((1000000 * Math.random()));
+        changeDisplayProperty('confirmation-message', 'block');
+    }
 }
